f2bd0b639bac547cf4777fc05d72cb44bccd360d
c0258603 <pratap.komitireddy@santander.co.uk>
2023-03-22 00:01:20 +0000

    updated workflow

--- src/main/java/com/example/springbootdocker/controller/HelloWorldController.java
+++ src/main/java/com/example/springbootdocker/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
     @RequestMapping("/hello")
     public String home() {
-        return "Hello Pratap komitireddy!";
+        return "Hello venkata pratap!";
     }
 
 }
